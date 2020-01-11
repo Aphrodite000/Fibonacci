77243a5d1daf4c2576061c71a9de3a7b2f58aeb5
aphrodite000 <153118976@qq.com>
2020-01-11 22:38:57 +0800

    fb

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
-        int a=0;
-        int b=1;
-        int c=a+b;
-        while(n>c){
-            a=b;
-            b=c;
-            c=a+b;
+    public int countWays(int x, int y) {
+        // write code here
+        //先x
+        //如果i+1，则j不变，如果i=x，则剩余光走j
+        //如果j+1，则i不变，如果j=y，则剩余光走i
+        int count=0;
+        for(int i=0,j=0;(i+j)>(x+y);i++,j++){
+            if(i++){
+                j=j;
+            }else{
+
+            }
         }
-        if(Math.abs(c-n)<Math.abs(n-b)){
-            System.out.println(Math.abs(n-c));
-        }else{
-            System.out.println(Math.abs(n-b));
-        }
+        return count;
     }
 }
